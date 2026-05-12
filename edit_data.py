45b7a2a946f371a610b3f31e723ebd424a43903b
Jaya Perkasa <nichyas27@gmail.com>
2026-05-12 23:37:06 +0700

    Integrate data cleaning and update data editor
    Added data cleaning functionality and updated data editor to use session state.

--- edit_data.py
+++ edit_data.py
@@ -1,16 +1,24 @@
-import pandas as pd
 import streamlit as st
 from components.readFile import readFile
+from pages_content.edit_data.cleaning_data import clean_data
 
 def app():
+
     with st.container(key="box-uploader"):
         df = readFile()
 
+        if df is not None:
+            st.session_state["df"] = df
+
     with st.container(key="box-table"):
-        st.data_editor(
-            data=df, 
-            num_rows='dynamic',
-            hide_index=True,
-            use_container_width=True,
-            width='stretch',
-        )
+        if "df" in st.session_state:
+            st.data_editor(
+                data=st.session_state["df"],
+                num_rows="dynamic",
+                hide_index=True,
+                use_container_width=True
+            )
+
+    with st.container(key="box-cleaning"):
+        if "df" in st.session_state:
+            clean_data(st.session_state["df"])
